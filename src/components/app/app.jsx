b267f55369361433a3ed02706d0c90d6832fc029
Max Salnikov <liss38@yandex.ru>
2019-10-16 11:44:51 +0300

    module1-task2: .jsx для импортов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import WelcomeScreen from './welcome-screen/welcome-screen';
+import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
-const App = () => {
-  return <WelcomeScreen />;
+const App = (props) => {
+  const {gameTime, errorCount} = props;
+
+  return <WelcomeScreen
+    time={gameTime}
+    errorCount={errorCount}
+  />;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/app';
+import App from './components/app/app.jsx';
 
 const init = () => {
+  const settings = {
+    gameTime: 9,
+    errorCount: 3,
+  };
+
   ReactDOM.render(
-      <App />,
+      <App
+        gameTime={settings.gameTime}
+        errorCount={settings.errorCount}
+      />,
       document.querySelector(`#root`)
   );
 };
