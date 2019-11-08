b508ed4e0e3c4123df7d1270bc846a23eddf7e07
Max Salnikov <liss38@yandex.ru>
2019-11-08 12:14:34 +0300

    module4-task1: аудиотреки с cooldj

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -5,22 +5,22 @@ export const questions = [
     answers: [
       {
         id: `id-1`,
-        src: `https://s86iva.storage.yandex.net/get-mp3/bc23a5873bff02797e64de3e54b0d4a4/000596c452cac739/rmusic/U2FsdGVkX1-jzCDiY-j4Em0ssIVAwz8YTKyQrRKdzTUYblSFdpKfKTNFx3il6Y4qax7BeV6t8QVhqIbPnPmE4FeCrctuQH4P3_azXpU0myo/70b0805e9721726f9a5b4e396254ed883031dbc97103b6702145f17c17c01cd6?track-id=17656988&play=false`,
+        src: `https://dl1.cool.dj/listen/62304134/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5R1RxRWIzejJFcDdyeHVOTE9GVUZ2YkJveWt6SjlrUVVWYmkrUFovbGtmY3o3NmM1Mzgxb2hJUVFmTnJOS2x5NU9xSEIvLzdFY25sVS9pdXg1WnhCYTQ/gayazovs-brothers-uvezite-menya-na-dip-haus_(Cool.DJ).mp3`,
         genre: `rock`,
       },
       {
         id: `id-2`,
-        src: `https://s32iva.storage.yandex.net/get-mp3/a36380a3b640a7097dd30a57b75eeb46/000596c4545a1383/rmusic/U2FsdGVkX1-3iaG68TpbyDT7AcEvl_Almk4e_QdciuRWQufsY0b1rKhupu6nGh2jlq0NaUGB4vOs3l0gm2W4YVNdIuZ1RTvVP9HqfuRM4OE/6cad03c9d741f8ce2b9eef7d9acadeafdf22b0fa4ef38472a524ac0130135059?track-id=17656993&play=false`,
+        src: `https://dl1.cool.dj/listen/63098301/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5RUhsTUhTeXVub2drZzU2ZGNvNUFhY29kRUVlS1hjdEFQSXZqZnUvWm9ucWhxdG1vaGhwOFZZRkE3TlpaV1lLOER5RHZlREUva0xrNlRqamlMMDlQQWw/the-hatters-vsyo-srazu_(Cool.DJ).mp3`,
         genre: `pop`,
       },
       {
         id: `id-3`,
-        src: `https://s78iva.storage.yandex.net/get-mp3/2c494f0c49de23cf7e3cbaf9a7a9804b/000596c455bab08c/rmusic/U2FsdGVkX18aTXWCkdNsSIkToYSTgfOlpYjEfBNg-XhfUTk7GUeuYYI11fKYyuiFJX5d4-bzzwZXYRSSR4X9qkIRTBugIBhuh1-v3Aa62Zs/a6ae6af301cbd05ee2d194bc43214b442b1741b609adf92b58ab2a24cbc439b4?track-id=17656981&play=false`,
+        src: `https://dl1.cool.dj/listen/54146400/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5RmdzeWU0MEluK2pUVkR1V0E5MUVkNmdsUW1VR0l0RVE1d2dTaFhSZFhES0phenJybjZqM094dWM2VVd4RVZjeVdjbE9Cdm9RQmljMXRYMGNYajFHYjE/niletto-lyubimka_(Cool.DJ).mp3`,
         genre: `jazz`,
       },
       {
         id: `id-4`,
-        src: `https://s02man.storage.yandex.net/get-mp3/a996da3ba742b519f5ca9007edc7f1a8/000596c4574f907c/rmusic/U2FsdGVkX19bMhvJ6Pe-5lebgrkvmBMbgCI5m27fvgpzo5-zcuzN1S1l_2I_mwidFK_6jGBUK62q10Fuirxyq3qIdZqYGLT7UBbosrmo_Uc/dffb3e64d65291c68782c547b6563748e45634172bc9ef1a9318bd96b1fe6a12?track-id=17656985&play=false`,
+        src: `https://dl1.cool.dj/listen/62253838/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5RmcwVHVNMHYyUDJWYTVBTTNsdzMyVkE2aWtKVTJ4cUJmSUpKZE1aMWhDcEl1MVNBclJVZWVra1F6dGtVWkU3U1VBY3RzVDllU21jTHRHbHNNY1pHY1k/boronina-kabluk_(Cool.DJ).mp3`,
         genre: `rock`,
       },
     ],
@@ -31,22 +31,22 @@ export const questions = [
     answers: [
       {
         id: `id-1`,
-        src: `https://s213myt.storage.yandex.net/get-mp3/140ba915e133c73ab3d13257ba519a48/000596c459f7590a/rmusic/U2FsdGVkX1975B4zhyJXTJeGYahv8i8IFln2FaYUa5iWvj908bja80Pjj-K2OGyuXg3vF9BpayjIpvv9nSPrZVSRI3kvA9xSIFQD5ghAUM8/8e46a1567a6938219ad2a19892594051d90ec1dc811024f6bbe5b652d4d91c08?track-id=17656989&play=false`,
+        src: `https://dl1.cool.dj/listen/57451972/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5R2pHZlNsSWw0VDU0MEg5VHNtUDhuellIbEd4YXZ0dXJuZTNWbUlLbFB3OUhjRE1Rem9qZVBURnVRb1RWRmM3Y1JHeXZLT3JneDhjU01XeFdxcmcrWDg/rasa-pchelovod_(Cool.DJ).mp3`,
         genre: `rock`,
       },
       {
         id: `id-2`,
-        src: `https://s80iva.storage.yandex.net/get-mp3/41ef7adaebb5edad9f1de762b2aa051e/000596c45b48c64e/rmusic/U2FsdGVkX19yhtBnUDhTVLLa_3OZVH56eO2jnhswYY5-iSaqX98Psi8Rgqf3pl1Ky9dBrcHq2EbAs4uBCqYim_Q2uwfWD6JxjnvYsVeDquQ/9813f172e0c9b4bb00990bfad214d59a7ce71d13fe53231cf4120300d3b2283b?track-id=17656984&play=false`,
+        src: `https://dl1.cool.dj/listen/54935990/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5R2wxMDJqdDZhWlYxL2Zkdm5ZQThZWWtpUTlQMDZUUnJRc0IyRW9qbzNSU1JMWlZWMEpnUzhTUWF1S1hmKzhNZTI2Y3lPZjVtYXZoZ0Faa1RwSHJ4bWU/tatyana-bulanova-igrayu-v-pryatki-na-sudbu_(Cool.DJ).mp3`,
         genre: `pop`,
       },
       {
         id: `id-3`,
-        src: `https://s44myt.storage.yandex.net/get-mp3/1ecd34900c883b7c4cc695c312729fcc/000596c45cc66074/rmusic/U2FsdGVkX183rmuTysy9EOdZE1FwVRaEYo-wRLV_-nEGXzooIpb79uuIA8OZ_URnBz60gQrN-B5QAOziIn9DFsdqF9PpddKzT1A2fpes26I/43eed3301531fd4411d8b7c66dadd8c07a00fe639ce34852554aca7e739bdd74?track-id=17656990&play=false`,
+        src: `https://dl1.cool.dj/listen/62940251/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5SFpjWEtURjYvWXMvVkZyYkFqVWY4R05wQlBYcnhWT2Viam56ZDlsMVRyK1ZONVNtWjBkSklGR3hlNDVGenJUQnh0ZndmUlUrSzJxTnBGR2RRd1hiQVM/little-big-rock-paper-scissors_(Cool.DJ).mp3`,
         genre: `jazz`,
       },
       {
         id: `id-4`,
-        src: `https://s69man.storage.yandex.net/get-mp3/b8498006af82a4e791fff661e0063d30/000596c45e408ea9/rmusic/U2FsdGVkX18Y459LJw7VO35PiawFI8eD65ay-rbg6eMwuDlydDaSrT4tdm0bL8pi5Zch7hCIuTf3-Kn_WLK76bQdhqZFzJMLRvULGRj2sUk/694656c9dc4af6309747dec61ef290a868697f2818c93dcdc53aa220f283b2dd?track-id=17656982&play=false`,
+        src: `https://dl1.cool.dj/listen/51307074/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5RkVMZ0k5RWd1dytEcVl6aktJcm42d05GS3lJWXFCdWhvWG12bnJqMXo0UmRPMGxYQlA4WWlJYlF5dXI5VmdmUHVZaFRqOHZ0eVFITHM2bnFySXU2dHk/saint-jhn-roses-imanbek-remix_(Cool.DJ).mp3`,
         genre: `rock`,
       },
     ],
@@ -55,7 +55,7 @@ export const questions = [
     type: `artist`,
     song: {
       artist: `Jim Beam`,
-      src: `https://s30man.storage.yandex.net/get-mp3/c049eadaa96c0fb05e2431e34c4b11a9/000596c45fb50f0e/rmusic/U2FsdGVkX1-d6Z0kSnL2lWZpczsXYHxJ_rdnJSuZBU7NailwSVYVBXBWlElF-oAxzs3BnFOSjcgIcGDkLe2WgR6OsaAd50fAV1RwUQTY9Lk/7df4a9a716c2601f3781541df1761fda8f3c7d69cefb9e63ea19f3e2e303a8b5?track-id=17656986&play=false`,
+      src: `https://dl1.cool.dj/listen/60679109/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5RnRxYVQrYWlINjQ4eFQ0dmFoZkIxZ3JqODhhU2VvdC9QQytLbHBwY3R0MEZvWThwSHlhTTVEZUd5cXZSanBLWDhuZEZSd3A2Nm1ZL3lGMEgwN3NlZko/audiosoulz-dancefloor_(Cool.DJ).mp3`,
     },
     answers: [
       {
@@ -79,7 +79,7 @@ export const questions = [
     type: `artist`,
     song: {
       artist: `Jack Daniels`,
-      src: `https://s17man.storage.yandex.net/get-mp3/c5f38f15737e112249a68db3744e8ae8/000596c4623493a2/rmusic/U2FsdGVkX1-xOpuAu8gEh8JzMJvm8UdVJs0ArdT0Tu1RoWWnPeKDVOgm1We6LFNMgmwWFISP24qEqJEZT80IcglheOZGALH31OmTPJqE3oM/31112d7e90fd49e4a6dfc1f01b340399c3cacac94e30a0c081fbc16a2e21d847?track-id=17656980&play=false`,
+      src: `https://dl1.cool.dj/listen/63285671/WG9NSnJ5QmFreFp5bFV1ZDlmRGErZDJXVWMzcnpGdWZjdUErL3haVzA5R1E5UXhRTnJPazUzQVNNZ29FR1FmejVSdXF0eDZZa3hsT0RuZmVNK1ZTcjc2Yngra3FqeXFuVmVUYzZuYXphbzFibkxialVUdUw1NktyaEdwTHJCT1M/lindemann-knebel_(Cool.DJ).mp3`,
     },
     answers: [
       {
